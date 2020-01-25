41c9780cb283a07a8f82615016c92e0cb2a8dddf
Nayee <j_nayee@ats-hr420-dm09.fc.ca>
2020-01-25 16:16:49 -0500

    update

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Nayee_Jay_SVG
+![It is monster icon!](images/orange.jpg "The logo")
+
+
+#Monster INC.
+
+Monster INC. is a set of monster icons with different moods and features.
+
+### Getting Started
+This Website is created by HTML/CSS/JS with AI designs.
+
+
+### Prerequisites
+Just an editor and Git!
+
+## Author
+Jay Nayee
+
+## License
+This project is licensed under the MLT license.
--- js/main.js
+++ js/main.js
@@ -4,30 +4,20 @@
 
 
 (() => {
-  console.log("some message - from Javascript");
-  // find the button element on page by its id with javascript
-  let
-  // theButton = document.querySelector("#myButton"),
-  theHeading = document.querySelector ('h1'),
-  // svgImage = document.querySelector("#svggraphic"),
-  allSVGs = document.querySelectorAll(".svg");
+  console.log("SVG ICONS- from Javascript");
 
-  // this function changes the heading text
-  function changeText() {
-   theHeading.textContent = "hello there from console";
- }
- // log the id of the svg to the console when a uer hovers on it
- function logsvg() {
-  console.log(this.id);
-}
+   let svgImage = document.querySelectorAll('.svg-img');
 
+   function logSVG()
+   {
+     console.log(this.id);
+   }
 
- // theButton.addEventListener("click", changeText);
- // svgImage.addEventListener("click", logsvg);
- // svgImage2.addEventListener("click", logsvg);
+   function clickEvent()
+   {
+     this.classList.toggle("selected");
+   }
 
- // loop through the collection and apply our event handling to each element
- allSVGs.forEach(item => item.addEventListener('click', logsvg));
-// document.querySelector('h1').textContent = "hello there from console";
+   svgImage.forEach(image => image.addEventListener('mouseover', logSVG));
 
 })();
